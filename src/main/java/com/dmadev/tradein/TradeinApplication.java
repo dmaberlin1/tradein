6d6ad20dabaa5e3d2af39ff8a403968e53d126ba
dmaberlin1 <dmaberlin77@gmail.com>
2023-11-26 21:39:34 +0200

    feat:start task

---
+++ src/main/java/com/dmadev/tradein/TradeinApplication.java
@@ -0,0 +1,13 @@
+package com.dmadev.tradein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeinApplication.class, args);
+	}
+
+}
