ec2c6e3f5960e84f54829becdafa99ff4d7157d7
dmaberlin1 <dmaberlin77@gmail.com>
2023-12-04 01:28:02 +0200

    feat: admin panel

--- src/main/java/com/dmadev/tradein/models/User.java
+++ src/main/java/com/dmadev/tradein/models/User.java
@@ -52,6 +52,11 @@ public class User implements UserDetails {
     }
 
 
+    //security
+    public boolean isAdmin(){
+        return roles.contains(Role.ROLE_ADMIN);
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
--- src/main/java/com/dmadev/tradein/services/UserService.java
+++ src/main/java/com/dmadev/tradein/services/UserService.java
@@ -7,6 +7,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -29,5 +36,38 @@ public class UserService {
         log.info("Saving new User with email:{}",email);
         userRepository.save(user);
         return true;
+    }
+
+    public List<User> list(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/user/ban/{id}")
+    public void banUser(Long id) {
+        User user=userRepository.findById(id).orElse(null);
+        if(user !=null){
+            if(user.isActive()){
+                user.setActive(false);
+                log.info("Ban user with id ={};email:{}",user.getId(),user.getEmail());
+
+            }else{
+                user.setActive(true);
+                log.info("Unban user with id ={};email:{}",user.getId(),user.getEmail());
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void changeUserRoles(User user, Map<String, String> form) {
+        //преобразуем все роли в строковый вид
+        Set<String>roles= Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        user.getRoles().clear();
+        for(String key :form.keySet()){
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+
     }
 }
